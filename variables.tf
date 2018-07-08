7852253b3eb76da7ab76647981c98618b3ce495e
Chris King (CSA - UK) <chkin@microsoft.com>
2018-07-08 11:39:54 +0100

    added service principal to variables file

--- variables.tf
+++ variables.tf
@@ -9,3 +9,12 @@ variable "tags" {
     env    = "training"
   }
 }
+
+variable "tenant_id" {
+  description = "Tenant ID for Azure AD Directory/Tenant"
+  default     = "72f988bf-86f1-41af-91ab-2d7cd011db47"
+}
+
+variable "kvr_object_id" {
+  default = "7d5e4914-4b01-445d-8da9-1ebce1d35715"
+}
